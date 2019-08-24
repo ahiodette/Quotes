130e1a1b33a5660d201822cbcf1cc562b6c310f9
odetteahishakiye <ahiode6@gmail.com>
2019-08-24 23:05:42 +0200

    Delete quotes

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -18,5 +18,4 @@ export class QuoteFormComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -47,12 +47,6 @@ export class QuoteComponent implements OnInit {
   //     this.quotes.splice(index, 1);
   //   }
   // }
-  addNewQuote(quote){
-    let quoteLength = this.quotes.length;
-    quote.id = quoteLength+1;
-    quote.completeDate = new Date(quote.completeDate)
-    this.quotes.push(quote)
-  }
   deleteQuote(isComplete, index){
     if (isComplete) {
       let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
@@ -62,6 +56,13 @@ export class QuoteComponent implements OnInit {
       }
     }
   }
+  addNewQuote(quote){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
+  }
+  
   constructor() { }
 
   ngOnInit() {
